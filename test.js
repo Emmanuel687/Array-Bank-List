89a617e3319a4cd2e32142c6fbea94eee3cd0082
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-01 20:24:06 +0300

    Created Card.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-// const funcManu = manu => {
-//   manu.map(man => {
-//     man * 6;
-//     const post = man <= 6 ? `Small` : `Large`;
-//     console.log(post);
+const funcManu = manu => {
+  manu.map(man => {
+    man * 6;
+    const post = man <= 6 ? `Small` : `Large`;
+    console.log(post);
 
-//     manu.find((man, i) => console.log(man<5)
-//     );
-//   });
-// };
+    manu.find((man, i) => console.log(man<5)
+    );
+  });
+};
 
-// console.log(funcManu([3, 4, 5, 6, 78, 8]));
+console.log(funcManu([3, 4, 5, 6, 78, 8]));
